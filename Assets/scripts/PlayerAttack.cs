35f0939151641976733b4fb1005574e92406d229
Tauha Imran <161915837+tauhaimran@users.noreply.github.com>
2025-10-01 18:59:52 +0500

    stufff

--- Assets/scripts/PlayerAttack.cs
+++ Assets/scripts/PlayerAttack.cs
@@ -5,14 +5,33 @@ using UnityEngine;
 public class PlayerAttack : MonoBehaviour
 {
     // Start is called before the first frame update
+    public PlayerMovement playerMovement;
+    private float coolDownTimer;
+    public float attackCoolDown;
+
+    public Animator animator;
+
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.Z) && playerMovement.canAttack() && coolDownTimer >= attackCoolDown)
+        {
+            //attack
+            Debug.Log("attack");
+            Attack();
+        }
+
+        coolDownTimer += Time.deltaTime;
+    }
+    
+    public void Attack()
+    {
+        //attack
+        Debug.Log("attacking");
     }
 }
